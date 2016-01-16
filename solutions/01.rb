bc5655e405225d6abb32c33188034072dfebe887
alex-nikolov <alexnickolow@gmail.com>
2016-01-16 13:05:12 +0200

    replace case statement with a clearer calculation

--- solutions/01.rb
+++ solutions/01.rb
@@ -6,12 +6,7 @@ EXCHANGE_RATE = {
 }
 
 def convert_to_bgn(price, currency)
-  price_in_bgn = case currency
-                   when :usd then price * 1.7408
-                   when :eur then price * 1.9557
-                   when :gbp then price * 2.6415
-                   else price
-                 end
+  price_in_bgn = price * EXCHANGE_RATE[currency]
   price_in_bgn.round(2)
 end
 
